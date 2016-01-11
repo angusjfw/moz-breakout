8e652118519e527711b51f218ee411ccd452bc32
hibreez <4ngus.whitehead@gmail.com>
2016-01-11 12:11:06 +0000

    Adds a paddle, collision logic and gameover alert.

--- src/game.js
+++ src/game.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-
   var canvas = document.getElementById("myCanvas");
   var ctx = canvas.getContext("2d");
 
@@ -7,28 +6,76 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var ballXPosition = canvas.width/2;
   var ballYPosition = canvas.height-30;
   var ballColor = "green";
-
   var dXball = 2;
   var dYball = -2;
 
+  var paddleHeight = 10;
+  var paddleWidth = 75;
+  var paddleX = (canvas.width-paddleWidth)/2;
+  var paddleSpeed = 7;
+
+  var rightPressed = false;
+  var leftPressed = false;
+  document.addEventListener("keydown", keyDownHandler, false);
+  document.addEventListener("keyup", keyUpHandler, false);
+ 
   function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    drawPaddle();
+    checkInput();
+    checkCollisions(moveBall);
+  }
+  setInterval(draw, 10);
+
+  function moveBall() {
     ballXPosition += dXball;
     ballYPosition += dYball;
+  }
 
-    if(ballXPosition + dXball > canvas.width-ballRadius  || ballXPosition + dXball < ballRadius) {
+  function checkCollisions(cb) {
+    checkTopAndSideCollisions();  
+    checkPaddleCollisions();
+    checkGameOver(cb);
+  }
+
+  function checkTopAndSideCollisions() {
+    if (ballXPosition + dXball > canvas.width-ballRadius  || ballXPosition + dXball < ballRadius) {
       dXball = -dXball;
       ballColor = getRandomColor();
     }
 
-    if(ballYPosition + dYball > canvas.height-ballRadius || ballYPosition  + dYball < ballRadius) {
+    if (ballYPosition  + dYball < ballRadius) {
       dYball = -dYball;
       ballColor = getRandomColor();
     }
   }
 
-  setInterval(draw, 10);
+  function checkPaddleCollisions() {
+    if (ballYPosition + dYball > (canvas.height-ballRadius) - paddleHeight) {
+      if(ballXPosition > paddleX && ballXPosition < paddleX + paddleWidth) {
+        dYball = -dYball;
+      }
+    }
+  }
+
+  function checkInput() {
+    if(rightPressed && paddleX < canvas.width-paddleWidth) {
+          paddleX += paddleSpeed;
+    }
+    else if(leftPressed && paddleX > 0) {
+          paddleX -= paddleSpeed;
+    }
+  }
+
+  function checkGameOver(cb) {
+    if (ballYPosition + dYball > canvas.height - ballRadius) {
+      alert("GAME OVER");
+      document.location.reload();
+    } else {
+      cb();
+    }
+  }
 
   function drawBall() {
     ctx.beginPath();
@@ -36,6 +83,32 @@ document.addEventListener("DOMContentLoaded", function(event) {
     ctx.fillStyle = ballColor;
     ctx.fill();
     ctx.closePath();
+  }
+
+  function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+      rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+      leftPressed = true;
+    }
+  }
+
+  function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+      rightPressed = false;
+    }
+    else if(e.keyCode == 37) {
+      leftPressed = false;
+    }
   }
 
   function getRandomColor() {
